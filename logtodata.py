a2495f687fa5b27e3c36185851b0c44cd7ac1079
lhalb <l.halbauer@web.de>
2022-04-06 07:41:57 +0200

    - added NC-Code and reworked loghandling

--- logtodata.py
+++ logtodata.py
@@ -1,25 +1,33 @@
-import os
+import os, fnmatch
 import re
 import pandas as pd
 
 if __name__ == "__main__":
     # Wo liegen die .MPF Logfiles
-    src = "data/datalogs"
+    src = r"C:\Users\halbauer\Desktop\logs"
 
     # Wo soll die Ergebnisdatei gespeichert werden
-    dest = "data"
+    dest = r"C:\Users\halbauer\Desktop\logs"
 
     # Ausgabename der vollständigen Exceldatei
     fname = 'results.xlsx'
 
     # Liste, in der Bezeichnung der Figuren enthalten ist
-    datalist = "overview_files.xlsx"
+    datalist = r"C:\Users\halbauer\Desktop\overview_files.xlsx"
+
+    # dictionary mit Spalten und Namen der auszuwertenden Kenngrößen
+    to_calc = {
+        'ram': 'figload',
+        'wrt': 'wrt',
+        'pc-1': 'pc_read_1',
+        'pc-2': 'pc_read_2'
+    }
 
     # Name der Spalten
-    names = ['nr', 'wdh', 'slot', 'ram', 'wrt']
+    names = ['nr', 'wdh', 'slot'] + list(to_calc.keys())
 
     # Dateien aus dem Quellverzeichnis auflisten
-    files = os.listdir(src)
+    files = fnmatch.filter(os.listdir(src), '*.MPF')
 
     # als Dataframe einlesen
     df_list = [pd.read_table(os.path.join(src, file), comment='#', sep=' ', header=None, names=names)
@@ -30,23 +38,29 @@ if __name__ == "__main__":
 
     # Gruppiere die Daten nach Nr und bilde von WRT und RAM
     # den Median
-    medians = big_df.groupby('nr')[['ram', 'wrt']].median()
+    medians = big_df.groupby('nr')[list(to_calc.keys())].median()
     # das Maximum
-    maxs = big_df.groupby('nr')[['ram', 'wrt']].max()
+    # maxs = big_df.groupby('nr')[['ram', 'wrt']].max()
     # Das Minimum
-    mins = big_df.groupby('nr')[['ram', 'wrt']].min()
+    # mins = big_df.groupby('nr')[['ram', 'wrt']].min()
 
     # benenne entsprechend
-    medians = medians.rename(columns={'ram': 'ram-med', 'wrt': 'wrt-med'})
-    maxs = maxs.rename(columns={'ram': 'ram-max', 'wrt': 'wrt-max'})
-    mins = mins.rename(columns={'ram': 'ram-min', 'wrt': 'wrt-min'})
+    medians = medians.rename(columns=to_calc)
+    # maxs = maxs.rename(columns={'ram': 'ram-max', 'wrt': 'wrt-max'})
+    # mins = mins.rename(columns={'ram': 'ram-min', 'wrt': 'wrt-min'})
 
-    medians['sum-med'] = medians.sum(axis=1)
+    medians['sum-med'] = medians[[to_calc['ram'], to_calc['wrt']]].sum(axis=1)
 
-    ergs = pd.concat([medians, maxs, mins], axis=1)
+    ergs = medians
+    # ergs = pd.concat([medians, maxs, mins], axis=1)
 
     df = pd.read_excel(datalist, index_col=2)
 
-    combined_df = pd.concat([df, ergs], axis=1)
+    try:
+        combined_df = pd.concat([df, ergs], axis=1)
+    except pd.errors.InvalidIndexError:
+        df['ft'] = df.index
+        df.index = list(range(1, len(df)+1))
+        combined_df = pd.concat([df, ergs], axis=1)
 
     combined_df.to_excel(os.path.join(dest, fname))
